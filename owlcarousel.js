0cdcf32c16cfb89c354719a2badb80605ff6355f
Arifuzzaman-Swapnil <21224103051@cse.bubt.edu.bd>
2025-01-07 06:32:36 +0600

    complete index.html

--- owlcarousel.js
+++ owlcarousel.js
@@ -1,11 +1,9 @@
 $(document).ready(function(){
-
     // banner owl carousel
     $("#banner-area .owl-carousel").owlCarousel({
         dots: true,
         items: 1
     });
-
     // top sale owl carousel
     $("#top-sale .owl-carousel").owlCarousel({
         loop: true,
@@ -23,20 +21,16 @@ $(document).ready(function(){
             }
         }
     });
-
     // isotope filter
     var $grid = $(".grid").isotope({
         itemSelector : '.grid-item',
         layoutMode : 'fitRows'
     });
-
     // filter items on button click
     $(".button-group").on("click", "button", function(){
         var filterValue = $(this).attr('data-filter');
         $grid.isotope({ filter: filterValue});
     })
-
-
     // new phones owl carousel
     $("#new-phones .owl-carousel").owlCarousel({
         loop: true,
@@ -54,7 +48,6 @@ $(document).ready(function(){
             }
         }
     });
-
     // blogs owl carousel
     $("#blogs .owl-carousel").owlCarousel({
         loop: true,
@@ -69,14 +62,11 @@ $(document).ready(function(){
             }
         }
     })
-
-
-    // product qty section
+    // product section
     let $qty_up = $(".qty .qty-up");
     let $qty_down = $(".qty .qty-down");
     // let $input = $(".qty .qty_input");
-
-    // click on qty up button
+    // click on up button
     $qty_up.click(function(e){
         let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
         if($input.val() >= 1 && $input.val() <= 9){
@@ -85,9 +75,8 @@ $(document).ready(function(){
             });
         }
     });
-
-       // click on qty down button
-       $qty_down.click(function(e){
+    // click on down button
+    $qty_down.click(function(e){
         let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
         if($input.val() > 1 && $input.val() <= 10){
             $input.val(function(i, oldval){
@@ -95,6 +84,4 @@ $(document).ready(function(){
             });
         }
     });
-
-
 });
