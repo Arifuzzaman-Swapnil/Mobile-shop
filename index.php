abf67c8b6f624580a36c6801496b7397d9ccbc24
Arifuzzaman-Swapnil <21224103051@cse.bubt.edu.bd>
2025-01-10 03:26:54 +0600

    completed index.php files

--- index.php
+++ index.php
@@ -5,7 +5,12 @@
 ?>
 
 <?php
-
+    include('php template/banner-area.php'); 
+    include('php template/top-sale.php'); 
+    include('php template/special-price.php'); 
+    include('php template/ads-section.php'); 
+    include('php template/new-phone.php'); 
+    include('php template/blogs.php'); 
 
 ?>
 
---
+++ php template/ads-section.php
@@ -0,0 +1,7 @@
+  <!-- Ads section start -->
+  <section id="banner_adds">
+        <div class="container py-5 text-center">
+          <img src="./assets/banner2-cr-500x150.jpg" alt="" class="img-fluid">
+          <img src="./assets/banner1-cr-500x150.jpg" alt="" class="img-fluid"></div>
+    </section>
+  <!-- Ads section end -->
